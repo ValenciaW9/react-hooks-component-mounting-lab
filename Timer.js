46568c8ab50b24e9acdaa6f70ec23dc41e5e3225
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-12-31 21:45:08 -0500

    react-hooks-component-mounting-lab/
    react-hooks-component-mounting-lab/

--- Timer.js
+++ Timer.js
@@ -3,24 +3,21 @@ import React, { Component } from "react";
 class Timer extends Component {
   state = {
     time: 0,
-    color: "#" + Math.floor(Math.random() * 16777215).toString(16)
+    color: "#" + Math.floor(Math.random() * 167772).toString(16)
   };
 
-  // add your code here
-
-  render() {
-    const { time, color } = this.state;
-    return (
-      <section className="Timer" style={{ background: color }}>
-        <h1>{time}</h1>
-        <button onClick={this.stopClock}>Stop</button>
-        <aside className="mountText">Mounted</aside>
-        <small onClick={this.handleClose}>X</small>
-      </section>
-    );
+  componentDidMount() {
+    this.handleAddTimer();
   }
 
-  //clock functions
+  componentWillUnmount() {
+    this.stopClock();
+  }
+
+  handleAddTimer = () => {
+    this.interval = setInterval(this.clockTick, 1000);
+  };
+
   clockTick = () => {
     this.setState(prevState => ({
       time: prevState.time + 1
@@ -31,10 +28,21 @@ class Timer extends Component {
     clearInterval(this.interval);
   };
 
-  // for the 'x' button,
   handleClose = () => {
-    this.props.removeTimer(this.props.id);
+    this.props.onRemove(this.props.id);
   };
+
+  render() {
+    const { time, color } = this.state;
+    return (
+      <section className="timer" style={{ background: color }}>
+        <h1>{time}</h1>
+        <button onClick={this.stopClock}>Stop</button>
+        <aside className="mounttext">Mounted</aside>
+        <small onClick={this.handleClose}>X</small>
+      </section>
+    );
+  }
 }
 
 export default Timer;
